c479c1db7b1a68749b4ca20eac37c72c7a360870
Samuel <smyles96@vt.edu>
2021-04-19 14:52:05 -0400

    Remove S3 artifact reference

--- scripts/create-codedeploy-stack.sh
+++ scripts/create-codedeploy-stack.sh
@@ -3,8 +3,9 @@ echo "[*] Creating CodeDeploy resources"
 aws cloudformation create-stack --stack-name node-app-codedeploy-resources --template-body file://cloudformation-templates/code-deploy.yml --capabilities CAPABILITY_IAM --parameters file://cloudformation-templates/parameters.json
 
 #wait for resources to be created
-echo waiting for successful creation of stack
 echo "[*] Waiting for resources to be created"
 aws cloudformation wait stack-create-complete --stack-name node-app-codedeploy-resources
+
+echo "[*] Resources created"
 
 exit
